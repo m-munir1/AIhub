823a577653d5f004f38bff64ce57c34067337f3b
MuhammadHamzaSaleemi <muhammadhamza0369@gmail.com>
2025-02-04 16:35:04 +0500

    home page completed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,21 @@
 import React from "react";
-import Login from "@/components/login";
 
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import Pricing from "@/components/pricing";
+import FAQSection from "@/components/faq";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/footer";
 export default function index() {
   return (
     <>
-      <Login />
+    <Navbar/>
+    <HeroSection/>
+    <Pricing/>
+    <FAQSection/>
+    <ContactSection/>
+    <Footer/>
+      {/* <Login /> */}
     </>
   );
 }
